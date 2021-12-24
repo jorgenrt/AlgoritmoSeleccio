692f1de7ea86c9f88f8eaa015aa64290218d8d5c
jorgerangel <77414914+jorgenrt@users.noreply.github.com>
2021-12-24 01:53:40 -0600

    Agregar archivos de proyecto.

---
+++ AlgoritmoSeleccion/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seleccion_Consola_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[100];
+            Random rnd = new Random();
+            int numero = rnd.Next(1, 100);
+            Console.WriteLine("Numeros a ordenar");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = rnd.Next(1, 100);
+            }
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine(vector[i]);
+            }
+
+           seleccion(vector);
+
+            Console.WriteLine("Vector ordenado");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write("{0} ", vector[i]);
+            }
+            Console.ReadKey();
+        }
+
+        private static void seleccion(int[] vector)
+        {
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < vector.Length; j++)
+                {
+                    if (vector[min] > vector[j])
+                    {
+                        min = j;
+
+                    }
+                }
+                int temp = vector[i];
+                vector[i] = vector[min];
+                vector[min] = temp;
+            }
+        }
+
+
+    }
+
+}
